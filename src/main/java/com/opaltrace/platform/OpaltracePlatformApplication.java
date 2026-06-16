a581464fc4f8471e5368f7503b1bfbe83ce24613
Rose Almendra <rosealmendra.calderon@gmail.com>
2026-06-15 23:41:12 -0500

    chore: initial commit

---
+++ src/main/java/com/opaltrace/platform/OpaltracePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.opaltrace.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpaltracePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpaltracePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/opaltrace/platform/OpaltracePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opaltrace.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpaltracePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
