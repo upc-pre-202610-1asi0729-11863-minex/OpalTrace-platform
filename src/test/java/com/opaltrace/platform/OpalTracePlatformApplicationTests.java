221fc8b8002582ee09a0f6a43b2b833f9411c4f1
Rose Almendra <rosealmendra.calderon@gmail.com>
2026-06-17 16:48:03 -0500

    chore: add spring profiles.

--- src/test/java/com/opaltrace/platform/OpalTracePlatformApplicationTests.java
+++ src/test/java/com/opaltrace/platform/OpalTracePlatformApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OpaltracePlatformApplicationTests {
+class OpalTracePlatformApplicationTests {
 
     @Test
     void contextLoads() {
